f002985f4a253ee2f510c0cbccb565a7730af352
Alex <alex.akten@outlook.com>
2025-05-17 22:10:39 +0200

    tm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,7 +82,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "reKindle",
+  title: "reKindle™",
   description: "Designed by reKindle™",
   openGraph: {
     images: [
