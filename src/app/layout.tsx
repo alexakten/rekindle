192f260ea2caaeb5bd12f944109221b28fb4259c
Alex <alex.akten@outlook.com>
2025-05-17 16:38:24 +0200

    no scroll over

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,37 +4,73 @@ import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 
 const redaction10 = localFont({
-  src: [{ path: "./fonts/redaction/Redaction_10-Regular.woff2", weight: "100", style: "normal" }],
+  src: [
+    {
+      path: "./fonts/redaction/Redaction_10-Regular.woff2",
+      weight: "100",
+      style: "normal",
+    },
+  ],
   variable: "--font-redaction-10",
   display: "swap",
 });
 
 const redaction20 = localFont({
-  src: [{ path: "./fonts/redaction/Redaction_20-Regular.woff2", weight: "20", style: "normal" }],
+  src: [
+    {
+      path: "./fonts/redaction/Redaction_20-Regular.woff2",
+      weight: "20",
+      style: "normal",
+    },
+  ],
   variable: "--font-redaction-20",
   display: "swap",
 });
 
 const redaction35 = localFont({
-  src: [{ path: "./fonts/redaction/Redaction_35-Regular.woff2", weight: "35", style: "normal" }],
+  src: [
+    {
+      path: "./fonts/redaction/Redaction_35-Regular.woff2",
+      weight: "35",
+      style: "normal",
+    },
+  ],
   variable: "--font-redaction-35",
   display: "swap",
 });
 
 const redaction50 = localFont({
-  src: [{ path: "./fonts/redaction/Redaction_50-Regular.woff2", weight: "50", style: "normal" }],
+  src: [
+    {
+      path: "./fonts/redaction/Redaction_50-Regular.woff2",
+      weight: "50",
+      style: "normal",
+    },
+  ],
   variable: "--font-redaction-50",
   display: "swap",
 });
 
 const redaction70 = localFont({
-  src: [{ path: "./fonts/redaction/Redaction_70-Regular.woff2", weight: "70", style: "normal" }],
+  src: [
+    {
+      path: "./fonts/redaction/Redaction_70-Regular.woff2",
+      weight: "70",
+      style: "normal",
+    },
+  ],
   variable: "--font-redaction-70",
   display: "swap",
 });
 
 const redaction100 = localFont({
-  src: [{ path: "./fonts/redaction/Redaction_100-Regular.woff2", weight: "100", style: "normal" }],
+  src: [
+    {
+      path: "./fonts/redaction/Redaction_100-Regular.woff2",
+      weight: "100",
+      style: "normal",
+    },
+  ],
   variable: "--font-redaction-100",
   display: "swap",
 });
@@ -56,7 +92,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} ${redaction10.variable} ${redaction20.variable} ${redaction35.variable} ${redaction50.variable} ${redaction70.variable} ${redaction100.variable}`}>
+    <html
+      lang="en"
+      className={` overscroll-none overflow-hidden ${inter.variable} ${redaction10.variable} ${redaction20.variable} ${redaction35.variable} ${redaction50.variable} ${redaction70.variable} ${redaction100.variable}`}
+    >
       <body>{children}</body>
     </html>
   );
