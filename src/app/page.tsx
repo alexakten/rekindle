4baf8b967dbec4074892afaa5a759fa9669ede0a
Alex <alex.akten@outlook.com>
2025-05-18 12:13:31 +0200

    renaming stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-// import Rekindle from "./components/Rekindle";
+import Rekindle from "./components/Rekindle";
 import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <main className="flex absolute bg-[#fafafa] select-none h-[100svh] w-screen items-center justify-center overflow-hidden">
       <Navbar />
-      {/* <Rekindle /> */}
+      <Rekindle />
     </main>
   );
 }
